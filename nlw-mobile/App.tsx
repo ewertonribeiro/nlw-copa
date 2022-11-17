10cfcaf3e1e5befbfee796e33d47a01ad3131c5d
Ewerton Ribeiro <ewertondosanjosz@gmail.com>
2022-11-17 19:03:13 -0300

    Correção de problemas com submodulo do git

---
+++ nlw-mobile/App.tsx
@@ -0,0 +1,31 @@
+import { NativeBaseProvider, Text, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import SignIn from "./src/Screens/SignIn";
+
+import { THEME } from "./src/styles/THEME";
+import Loading from "./src/Components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
